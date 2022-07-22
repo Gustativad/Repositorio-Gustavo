918b69db4162e42fa914a2c093725774917c4f36
Gustativad <gr219436@gmail.com>
2022-07-21 23:32:44 -0300

    Adição da Classe Resultados na pasta Models juntamente a anotações

--- WebTeste/Migrations/20220721002934_WebTeste.cs
+++ WebTeste/Migrations/20220721002934_WebTeste.cs
@@ -14,7 +14,7 @@ namespace WebTeste.Migrations
                 {
                     id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Jogador = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Inserir_Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Inserir_Data = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Inserir_Pontuação = table.Column<int>(type: "int", nullable: false)
                 },
---
+++ WebTeste/Models/Resultados.cs
@@ -0,0 +1,33 @@
+﻿namespace WebTeste.Models
+{
+    public class Resultados
+    {
+        //Nesta classe estaria sendo armazenada a parte do código responsável pela exibição de algumas resoluções pedidas como (total de pontos, média por jogo, maior e menor pontuação e etc.).
+
+        //Seguindo a instrução dada no documento do teste, deixarei as anotações sobre minha dificuldade com esta parte do código.
+
+        //Primeiro tive um pouco de dificuldade em achar uma forma de como conseguir retirar informações especificas do banco de dados para exibir na tela.
+
+        //O exemplo mais claro que posso citar seria o de pedir ao banco de dados que me retornasse somente os valores inseridos na coluna de "Inserir_Pontuação";
+        //Logo após deveria executar a parte do código responsável por fazer a soma total dos valores inseridos, o que retornaria o valor total que deveria ser exibido para o usuário.
+
+        //O mesmo problema se aplica aos demais resultados quando penso em retirar apenas os valores especificos que desejo para o resultado final.
+
+        //A lógica por trás dos resultados não foi tão complicada de entender pois no próprio documento havia pequenas descrições sobre os resultados.
+
+        //Assim como o total de pontos que deveria ser a soma de todos os valores.
+
+        //A média de pontos por jogo que deveria conter o total da soma dos valores da pontução e seria divido pelo total de jogos disputados pelo usuário, assim retornaria a média de pontos por jogo.
+
+
+        //Também senti um pouco de dificuldade para entender como poderia retirar o menor e o maior valor registrado nas pontuações.
+
+        //A solução que cheguei a pensar para resolver a questão de puxar os valores do banco de dados, foi a de utilizar o comando "Select" do SQL para especificar os dados que eu desejava;
+        //Porém não encontrei uma maneira de utilizar esse comando SQL dentro do código em C#, mesmo com as extensões do SQLServer adicionadas na máquina.
+
+        //Também pensei na possibilidade de usar o sistema de Chave Estrangeira do banco de dados, porém estive na dúvida sobre a eficácia deste método;
+        //Pois imaginei que para ter êxito na aplicação a chave estrangeira devesse ter sido criada junto do banco de dados lá no começo.
+
+        //Estes foram os pontos que me incomodaram e me deram uma pequena dificuldade para finalizar o código.
+    }
+}
